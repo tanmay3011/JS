1a317fa67db5ce18cec7a7ac28eb1ad8c406d1dd
tanmay3011 <tanmay3011@gmail.com>
2014-06-05 18:41:38 +0530

    Exercise2 Submission

--- Exercise2/exercise.js
+++ Exercise2/exercise.js
@@ -1,11 +1,31 @@
 /*jslint browser: true, devel: true */
-function checkAll(bool) {
+function CheckChoice() {
   "use strict";
-  var checkBox = document.getElementsByName("color");
-  var i=checkBox.length-1;
-  if (i > -1) {
-    do {
-      checkBox[i].checked = bool;
-    }while(--i >= 0);
-  }
+  this.checkBox = document.getElementsByName("color");
+  this.selectAll = document.getElementById("checkall");
+  this.noneSelect = document.getElementById("checknone");
+  this.check = function (choice) {
+    this.choice = choice;
+    this.i = this.checkBox.length - 1;
+    if (this.i > -1) {
+      do {
+        this.checkBox[this.i].checked = this.choice;
+        this.i = this.i - 1;
+      } while (this.i >= 0);
+    }
+  };
 }
+
+CheckChoice.prototype.events = function () {
+  "use strict";
+  var checkValue = this;
+  this.selectAll.onclick = function () {
+    checkValue.check(1);
+  };
+  this.noneSelect.onclick = function () {
+    checkValue.check(0);
+  };
+};
+
+var checkBoxObject = new CheckChoice();
+checkBoxObject.events();
