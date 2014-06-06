1679611b0d70ff0d1aa4ee1bd0213ae9518b15c9
tanmay3011 <tanmay3011@gmail.com>
2014-06-06 14:34:20 +0530

    Exercise 2c

--- Exercise2/exercise.js
+++ Exercise2/exercise.js
@@ -1,34 +1,38 @@
 /*jslint browser: true, devel: true */
 function CheckChoice() {
   "use strict";
-  var checkBoxes = [], selectAll = [], noneSelect = [];
-  this.checkBoxes = checkBoxes;
-  this.selectAll = selectAll;
-  this.noneSelect = noneSelect;
-
-  this.init = function () {
-    var checkValue = this;
-    this.selectAll.onclick = function () {
-      checkValue.check(1);
-    };
-    this.noneSelect.onclick = function () {
-      checkValue.check(0);
-    };
+  this.checkBoxes = this.selectAll = this.noneSelect = [];
+  this.init = function (checkBoxes, selectAll, noneSelect) {
+    this.checkBoxes = checkBoxes;
+    this.selectAll = selectAll;
+    this.noneSelect = noneSelect;
+    this.bind_events();
   };
 
-  this.check = function (choice) {
+  this.check = function (bool) {
     var i = this.checkBoxes.length - 1;
     if (i > -1) {
       do {
-        this.checkBoxes[i].checked = choice;
+        this.checkBoxes[i].checked = bool;
         i -= 1;
       } while (i >= 0);
     }
   };
 }
 
-var checkChoice = new CheckChoice();
-checkChoice.checkBoxes = document.getElementsByName("inputColor");
-checkChoice.selectAll = document.getElementById("checkAll");
-checkChoice.noneSelect = document.getElementById("checkNone");
-checkChoice.init();
+CheckChoice.prototype.bind_events = function () {
+  "use strict";
+  var checkValue = this;
+  this.selectAll.onclick = function () {
+    checkValue.check(true);
+  };
+  this.noneSelect.onclick = function () {
+    checkValue.check(false);
+  };
+};
+
+window.onload = function () {
+  "use strict";
+  var checkBoxes = document.getElementsByName("inputColor"), selectAll = document.getElementById("checkAll"), noneSelect = document.getElementById("checkNone"), checkChoice = new CheckChoice();
+  checkChoice.init(checkBoxes, selectAll, noneSelect);
+};
