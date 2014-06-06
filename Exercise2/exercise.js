2d4c8bc9ac9355fced40243811154db0416b9004
tanmay3011 <tanmay3011@gmail.com>
2014-06-06 12:58:19 +0530

    Exercise 2b

--- Exercise2/exercise.js
+++ Exercise2/exercise.js
@@ -1,30 +1,34 @@
 /*jslint browser: true, devel: true */
 function CheckChoice() {
   "use strict";
-  this.checkBox = document.getElementsByName("inputColor");
-  this.selectAll = document.getElementById("checkAll");
-  this.noneSelect = document.getElementById("checkNone");
-  this.events();
+  var checkBoxes = [], selectAll = [], noneSelect = [];
+  this.checkBoxes = checkBoxes;
+  this.selectAll = selectAll;
+  this.noneSelect = noneSelect;
+
+  this.init = function () {
+    var checkValue = this;
+    this.selectAll.onclick = function () {
+      checkValue.check(1);
+    };
+    this.noneSelect.onclick = function () {
+      checkValue.check(0);
+    };
+  };
+
   this.check = function (choice) {
-    var i = this.checkBox.length - 1;
+    var i = this.checkBoxes.length - 1;
     if (i > -1) {
       do {
-        this.checkBox[i].checked = choice;
+        this.checkBoxes[i].checked = choice;
         i = i - 1;
       } while (i >= 0);
     }
   };
 }
 
-CheckChoice.prototype.events = function () {
-  "use strict";
-  var checkValue = this;
-  this.selectAll.onclick = function () {
-    checkValue.check(1);
-  };
-  this.noneSelect.onclick = function () {
-    checkValue.check(0);
-  };
-};
-
 var checkChoice = new CheckChoice();
+checkChoice.checkBoxes = document.getElementsByName("inputColor");
+checkChoice.selectAll = document.getElementById("checkAll");
+checkChoice.noneSelect = document.getElementById("checkNone");
+checkChoice.init();
