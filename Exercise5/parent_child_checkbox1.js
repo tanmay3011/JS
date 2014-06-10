68aa81b565dbc40fbea99ac96ba1e1e543603680
tanmay3011 <tanmay3011@gmail.com>
2014-06-10 09:36:52 +0530

    Commit after Exercise 5

--- Exercise5/parent_child_checkbox1.js
+++ Exercise5/parent_child_checkbox1.js
@@ -1,4 +1,6 @@
-var CheckEvent = function(id) {
+/*jslint browser: true, devel: true */
+var CheckEvent = function (id) {
+  "use strict";
   this.parentCheckBox = document.getElementById(id);
   this.subCheckBoxes = document.getElementsByName(id + "Sub");
   this.subBlockCheckBoxes = document.getElementById(id + "List");
@@ -6,20 +8,22 @@ var CheckEvent = function(id) {
   this.bindEvents();
 };
 
-CheckEvent.prototype.subCheckBoxesEvent = function(choice) {
-  var length = this.subCheckBoxes.length;
-  for (i = 0; i < length; i++) {
-    this.subCheckBoxes[i].checked = choice;
+CheckEvent.prototype.subCheckBoxesEvent = function (choice) {
+  "use strict";
+  var length = this.subCheckBoxes.length, subCheckBoxCounter = 0;
+  for (subCheckBoxCounter = 0; subCheckBoxCounter < length; subCheckBoxCounter++) {
+    this.subCheckBoxes[subCheckBoxCounter].checked = choice;
   }
   this.subBlockCheckBoxes.style.display = (choice) ? "block" : "none";
   this.parentCheckBox.scrollIntoView(choice);
-}
+};
 
 //method to bind event on click
-CheckEvent.prototype.bindEvents = function() {
+CheckEvent.prototype.bindEvents = function () {
+  "use strict";
   var that = this;
-  this.parentCheckBox.onclick = function() { that.subCheckBoxesEvent(that.parentCheckBox.checked); };
-}
+  this.parentCheckBox.onclick = function () { that.subCheckBoxesEvent(that.parentCheckBox.checked); };
+};
 
 window.onload = function () {
   "use strict";
@@ -27,4 +31,4 @@ window.onload = function () {
   var checkEventDrinks = new CheckEvent("drink");
   var checkEventMovies = new CheckEvent("movie");
   var checkEventBikes = new CheckEvent("bike");
-}
+};
