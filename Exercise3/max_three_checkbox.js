d33a6230bee4eab2310c18f4cca8e026cce567b1
tanmay3011 <tanmay3011@gmail.com>
2014-06-12 21:15:06 +0530

    Exercise recommit

--- Exercise3/max_three_checkbox.js
+++ Exercise3/max_three_checkbox.js
@@ -2,18 +2,14 @@
 /*jslint browser: true, devel: true */
 var Days = function () {
   "use strict";
-  this.checkedCount = 0;
   this.checkedList = [];
-  this.checkedIndexList = [];
-  this.checkedIndexList2 = [];
+  this.checkedListIndex = [];
   this.minimumCheckLimit = 3;
   //method for initiation
   this.init = function (tableIdRef, noneSelectRef, checkBoxesRef) {
     this.tableId = tableIdRef;
     this.noneSelect = noneSelectRef;
     this.checkBoxes = checkBoxesRef;
-    this.totalNumberCheckBoxes = this.checkBoxes.length - 1;
-    this.bindEvents();
   };
 };
 
@@ -27,97 +23,57 @@ Days.prototype.popAlert = function () {
 Days.prototype.checkNone = function () {
   "use strict";
   var checkBoxesCounter;
-   //running this loop once and only unchecking the checked elements
-  for (checkBoxesCounter = 0; checkBoxesCounter < this.minimumCheckLimit; checkBoxesCounter++) {
-    this.checkBoxes[this.checkedIndexList2[checkBoxesCounter]].checked = false;
+  for (checkBoxesCounter = 0; checkBoxesCounter < this.checkedListIndex.length; checkBoxesCounter++) {
+    this.checkBoxes[this.checkedListIndex[checkBoxesCounter]].checked = false;
   }
   this.checkedList.length = 0;
-  this.checkedCount = 0;
 };
 
 //method to check the main functionality of checking three days
-Days.prototype.checkCount = function () {
+Days.prototype.checkBoxEvent = function (event) {
   "use strict";
-  var checkBoxesCounter;
-  this.checkedCount = 0;
-  this.checkedIndexList.length = 0;
-  this.noneSelect.checked = false;
-  for (checkBoxesCounter = 0; checkBoxesCounter <= this.totalNumberCheckBoxes; checkBoxesCounter++) {
-    if (this.checkBoxes[checkBoxesCounter].checked === true) {
-      this.checkedCount += 1;
-      this.checkedIndexList.push(checkBoxesCounter);
-    }
-    if (this.checkCount === this.minimumCheckLimit + 1) {      //condition for stopping this loop after getting the 4th checked element
-      break;
-    }
-  }
-  this.checkCountBasedSelection();
-};
-
-//method to select what to do next based on checkCount
-Days.prototype.checkCountBasedSelection = function () {
-  "use strict";
-  if (this.checkedCount === this.minimumCheckLimit) {
-    this.pushDays();
-  } else if (this.checkedCount < this.minimumCheckLimit) {
-    this.checkedList.length = 0;
-  } else {
-    this.checkedCount -= 1;
-    this.uncheckFourthClickDifference();
-    this.popAlert();
-  }
-}
-
-//method to uncheck the fourth click
-Days.prototype.uncheckFourthClickDifference = function() {
-  "use strict";
-  var differenceArray=[],diff=[];
-  for (var i=0; i<this.checkedIndexList2.length; i++) {
-    differenceArray[this.checkedIndexList2[i]]=true;
-  }
-  for (var i=0; i<this.checkedIndexList.length; i++) {
-    if (differenceArray[this.checkedIndexList[i]]) {
-      delete differenceArray[this.checkedIndexList[i]];
+  if (event.target.checked) {
+    if (event.target.value === "None") {
+      this.checkNone();
+    } else if (this.checkedList.length !== this.minimumCheckLimit) {
+      this.pushDays(event);
     } else {
-      differenceArray[this.checkedIndexList[i]] = true;
-      diff = this.checkedIndexList[i];
+      event.target.checked = false;
+      this.popAlert();
+    }
+  } else {
+    if (event.target.value !== "None") {
+      this.popDays(event);
     }
   }
-  this.checkBoxes[diff].checked = false;    //4th click option unchecked
 };
 
-//method to push days in array(optimized by only checking the checked item stoed in the checkedIndexList array)
-Days.prototype.pushDays = function(choice) {
+//method to push days into array when checked
+Days.prototype.pushDays = function (event) {
   "use strict";
-  var length = this.checkedIndexList.length;
-  this.checkedCount = 0;
-  this.checkedList.length = 0;
-  this.checkedIndexList2.length = 0;
-  for (var checkBoxesCounter = 0 ; checkBoxesCounter < length; checkBoxesCounter++) {
-     this.checkBoxes[this.checkedIndexList[checkBoxesCounter]].checked = true;
-     this.checkedIndexList2.push(this.checkedIndexList[checkBoxesCounter]) ;
-     this.checkedList.push(this.checkBoxes[this.checkedIndexList[checkBoxesCounter]].value);
+  this.noneSelect.checked = false;
+  this.checkedList.push(event.target.value);
+  this.checkedListIndex.push(event.target.id);
+};
+
+//method to pop days from array when unchecked
+Days.prototype.popDays = function (event) {
+  "use strict";
+  var popIndex;
+  popIndex = this.checkedList.indexOf(event.target.value);
+  if (popIndex !== -1) {
+    this.checkedList.splice(popIndex, 1);
+    this.checkedListIndex.splice(popIndex, 1);
   }
-};
-
-//method to bind event on click
-Days.prototype.bindEvents = function () {
-  "use strict";
-  var checkChoice = this;
-  this.tableId.onclick = function () {
-    checkChoice.checkCount();
-  };
-  this.noneSelect.onclick = function () {
-    checkChoice.checkNone();
-  };
 };
 
 //method to run javascript after loading whole page (mentioned script in head portion)
 window.onload = function () {
   "use strict";
-  var tableIdRef = document.getElementById("checkBoxTable");
-  var noneSelectRef = document.getElementById("checkNone");
-  var checkBoxesRef = document.getElementsByName("dayCheckBox");
+  var gridElement = document.getElementById("grid");
+  var noneSelectElement = document.getElementById("7");
+  var checkBoxesElement = document.getElementsByName("dayCheckBox");
   var days = new Days();
-  days.init(tableIdRef, noneSelectRef, checkBoxesRef);
+  days.init(gridElement, noneSelectElement, checkBoxesElement);
+  gridElement.addEventListener('click', function (event) { days.checkBoxEvent(event); }, false);
 };
