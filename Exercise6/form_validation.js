99a27e3b6d74e79ee143103d3b50e8754bac03b6
tanmay3011 <tanmay3011@gmail.com>
2014-06-14 10:16:41 +0530

    Exercise6 commit

--- Exercise6/form_validation.js
+++ Exercise6/form_validation.js
@@ -1,76 +1,111 @@
-/* Form Validation*/
+/*Validation of form*/
 /*jslint browser: true, devel: true */
-var Form = function (id) {
+var Form = function (formElement, textFieldsElement, selectTimeZoneElement, aboutMeElement, notificationCheckElement) {
   "use strict";
-  this.textFieldElement = document.getElementsByName('textinput');
-  this.textAreaElement = document.getElementById('textbox');
-  this.checkBoxElement = document.getElementById('checkbox');
-  this.selectElement = document.getElementById('TimeZone');
-  var that = this;
-  document.getElementById(id).onsubmit = function () { return that.validateEvents(); };
+  this.form = formElement;
+  this.textFields = textFieldsElement;
+  this.selectTimeZone = selectTimeZoneElement;
+  this.aboutMe = aboutMeElement;
+  this.notificationCheck = notificationCheckElement;
+  this.flag = true;
+  this.errors = [];
 };
 
-//to check empty that elements 
-Form.prototype.validateTextFieldElements = function () {
+//method to validate textfield inputs
+Form.prototype.validateTextFields = function () {
   "use strict";
-  var textFieldCounter = 0;
-  var valid = true;
-  for (textFieldCounter =  0; textFieldCounter < this.textFieldElement.length - 1; textFieldCounter++) {
-    if (this.textFieldElement[textFieldCounter].value === "") {
-      alert(this.textFieldElement[textFieldCounter].id + " cannot be blank");
-      valid = false;
-      break;
+  var textFieldsCounter;
+  for (textFieldsCounter = 0; textFieldsCounter < this.textFields.length; textFieldsCounter = textFieldsCounter + 1) {
+    this.textFields[textFieldsCounter].value = this.textFields[textFieldsCounter].value.trim();
+    if (!this.textFields[textFieldsCounter].value) {
+      this.errors.push(this.textFields[textFieldsCounter].name + " can not be empty");
+      this.textFields[textFieldsCounter].focus();
+      this.flag = false;
     }
   }
-  return valid;
 };
 
-//method to validate textarea input
-Form.prototype.validateTextAreaElement = function () {
+//method to validate AboutMe Text
+Form.prototype.validateAboutMe = function () {
   "use strict";
-  var textAreaMaxLimit = 50;
-  var valid = true;
-  if (this.textAreaElement.value === "") {
-    alert(this.textAreaElement.id + "blank");
-    valid = false;
+  this.aboutMe.value = this.aboutMe.value.trim();
+  if (this.aboutMe.value.length < 50) {
+    if (!this.aboutMe.value) {
+      this.errors.push(this.aboutMe.name + " cannot be empty");
+    } else {
+      this.errors.push("Minimum length of this box is 50");
+    }
+    this.aboutMe.focus();
+    this.flag = false;
   }
-  if (this.textAreaElement.value.length > textAreaMaxLimit) {
-    alert(this.textAreaElement.id + " exceeds the maximum count of 50");
-    valid = false;
-  }
-  return valid;
 };
 
-//method to valide checkbox input
-Form.prototype.validateCheckBoxElement = function () {
+//method to validate selectbox input
+Form.prototype.validateSelectTimeZone = function () {
   "use strict";
-  var valid = true;
-  if (!this.checkBoxElement.checked) {
-    alert("Do you really wanna leave it unchecked " + this.checkBoxElement.id);
-    valid = false;
+  if (this.selectTimeZone.options[this.selectTimeZone.selectedIndex].text === "GMT") {
+    this.errors.push("Please Select a Time Zone");
+    this.flag = false;
+    this.selectTimeZone.focus();
+    this.flag = false;
   }
-  return valid;
 };
 
-//method to valide selectbox input
-Form.prototype.validateSelectElement = function () {
+//method to validate notification checkbox checked or not
+Form.prototype.validateNotificationCheck = function () {
   "use strict";
-  var valid = true;
-  if (this.selectElement.options[this.selectElement.selectedIndex].text === "GMT") {
-    alert("Please Select a Time Zone " + this.selectElement.id);
-    valid = false;
+  if (this.notificationCheck.checked === false) {
+    this.errors.push(this.notificationCheck.name + " must be checked ");
+    this.notificationCheck.focus();
+    this.flag = false;
   }
-  return valid;
-}
+};
 
-Form.prototype.validateEvents = function () {
+//method to throw error
+Form.prototype.throwErrors = function () {
   "use strict";
-  var valid = this.validateTextFieldElements() && this.validateTextAreaElement() && this.validateCheckBoxElement() && this.validateSelectElement();
-  return valid;
+  var errorCounter;
+  for (errorCounter = 0; errorCounter < this.errors.length; errorCounter = errorCounter + 1) {
+    alert(this.errors[errorCounter]);
+  }
+  //this.errors.length = 0;
+};
+
+//method to show submit confirmation
+Form.prototype.sendConfirmation = function () {
+  "use strict";
+  alert("Form Submitted");
+};
+
+//method to validate
+Form.prototype.validate = function () {
+  "use strict";
+  this.validateTextFields();
+  this.validateSelectTimeZone();
+  this.validateAboutMe();
+  this.validateNotificationCheck();
+  if (this.errors.length === 0) {
+    this.sendConfirmation();
+    this.form.submit();
+  } else {
+    this.throwErrors();
+  }
+};
+
+var formHandler = function () {
+  "use strict";
+  var textFieldsElement = document.getElementsByClassName('TextFields');
+  var selectTimeZoneElement = document.getElementById('timeZone');
+  var aboutMeElement = document.getElementById('aboutMe');
+  var notificationCheckElement = document.getElementById('notification');
+  var formElement = document.getElementById('registerationForm');
+  var form = new Form(formElement, textFieldsElement, selectTimeZoneElement, aboutMeElement, notificationCheckElement);
+  form.validate();
 };
 
 //method to run javascript after loading whole page (mentioned script in head portion)
 window.onload = function () {
   "use strict";
-  var registrationform = new Form("userRegistrationForm");
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', formHandler, false);
 };
