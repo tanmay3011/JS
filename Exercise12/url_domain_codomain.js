af2b42316e7e08f6b0ebaee45d9d50e362b69e31
tanmay3011 <tanmay3011@gmail.com>
2014-06-16 13:10:46 +0530

    All exercise till 13

--- Exercise12/url_domain_codomain.js
+++ Exercise12/url_domain_codomain.js
@@ -1,72 +1,84 @@
 /*Find Domain SubDomain*/
 /*jslint browser: true, devel: true */
-var UrlDomainSubDomain = function (formElement, inputedElement) {
-  "use strict";
-  this.url = inputedElement;
-  this.domainName;
-  this.subDomainName;
-  this.form = formElement;
-  this.regexUrl = /(http[s]?\:\/\/)?(w{3}.)?([\w]+)(\.)?(\.)?([a-z.]{2,6})?([a-z.]{2,6})(\/)?([\w]+\/?)*$/;
-  this.flag = true;
+var constant = {
+  regexUrl :  /(http[s]?\:\/\/)?(w{3}.)?([\w]+)(\.)?(\.)?([a-z.]{2,6})?([a-z.]{2,6})(\/)?([\w]+\/?)*$/
 };
 
-//method number whether number matches with the regex or not
-UrlDomainSubDomain.prototype.checkUrlValidity = function () {
+var UrlDomainSubDomain = function () {
   "use strict";
-  this.url.value = this.url.value.trim();
-  if (!this.regexUrl.test(this.url.value)) {
-    alert("Wrong Url Entered! Please Enter a new url");
-    this.flag = false;
-  } else {
-    this.findDomainSubDomain();
-  }
+  this.url;
+  this.domainName;
+  this.subDomainName;
+  this.form;
+  this.submitButton;
+  this.init();
+};
+
+//attached event on submit click
+UrlDomainSubDomain.prototype.init = function (e) {
+  "use strict";
+  this.submitButton = document.getElementById('submitButton');
+  this.form = document.getElementById('urlForm');
+  this.bindEvent();
 };
 
 //method to find domain codomain
 UrlDomainSubDomain.prototype.findDomainSubDomain = function () {
   "use strict";
-  var hostname = (this.url.value.match(this.regexUrl));
-  if(!hostname[6]) {
-    this.domainName = hostname[3] + hostname[4] + hostname[7];
+  alert((!RegExp.$6) ? 'Domain: ' + RegExp.$3 + RegExp.$4 + RegExp.$7 : 'Subdomain: ' + RegExp.$3 + ' Domain: ' + RegExp.$6 + RegExp.$7);
+};
+
+//method number whether number matches with the regex or not
+UrlDomainSubDomain.prototype.checkUrlValidity = function (e) {
+  "use strict";
+  this.url = document.getElementById('url');
+  if (!constant.regexUrl.test(this.url.value)) {
+    alert("Wrong Url Entered! Please Enter a new url");
+    //e.preventDefault();
   } else {
-    this.domainName = hostname[6] + hostname[7];
-    this.subDomainName = hostname[3];
+    this.findDomainSubDomain();
   }
 };
 
-//method to validate number
-UrlDomainSubDomain.prototype.validate = function () {
+//to bind event on submit
+UrlDomainSubDomain.prototype.bindEvent = function () {
   "use strict";
-  this.checkUrlValidity();
-  if (this.flag) {
-    this.popAlert();
-    this.form.submit();
-  }
+  var that = this;
+  this.submitButton = document.getElementById('submitButton');
+  this.cb_addEventListener(this.form, 'submit', this.checkUrlValidity);
+  //this.form.addEventListener('submit', function (event) { that.checkUrlValidity(event); }, false);
 };
 
-//method to display result
-UrlDomainSubDomain.prototype.popAlert = function () {
+
+UrlDomainSubDomain.prototype.cb_addEventListener = function (obj, evt, fnc) {
   "use strict";
-  if(!this.subDomainName) {
-    alert("Domain: " + this.domainName);  
+  if (obj.addEventListener) {
+    console.log(obj);
+    obj.addEventListener(evt, fnc, false);
+  return true;
+  } else if (obj.attachEvent) {
+    return obj.attachEvent('on' + evt, fnc);
   } else {
-    alert("Domain: " + this.domainName + " & Codomain: " + this.subDomainName)
+    evt = 'on'+evt;
+    if(typeof obj[evt] === 'function') {
+      fnc = (function(f1,f2) {
+        return function() {
+          f1.apply(this,arguments);
+          f2.apply(this,arguments);
+        }
+      })(obj[evt], fnc);
+    }
+    obj[evt] = fnc;
+    return true;
   }
-};
-
-//attached event on submit click
-var formHandler = function (e) {
-  "use strict";
-  var inputedElement = document.getElementById('url'),
-      formElement = document.getElementById('urlForm'),
-      urlDomainSubDomain = new UrlDomainSubDomain(formElement, inputedElement);
-  e.preventDefault();
-  urlDomainSubDomain.validate();
+  return false;
 };
 
 //method to run javascript after loading whole page (mentioned script in head portion)
 window.onload = function () {
   "use strict";
-  var submitButton = document.getElementById('submitButton');
-  submitButton.addEventListener('click', formHandler, false);
+  var urlDomainSubDomain = new UrlDomainSubDomain();
 };
+
+
+
