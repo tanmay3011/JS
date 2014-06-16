7afb7f08538536c3e16b926e998f867d4f64a09a
tanmay3011 <tanmay3011@gmail.com>
2014-06-16 13:38:13 +0530

    All exercise till 13

---
+++ Exercise12/url_domain_subdomain.js
@@ -0,0 +1,58 @@
+/*Find Domain SubDomain*/
+/*jslint browser: true, devel: true */
+var constant = {
+  regexUrl :  /(http[s]?\:\/\/)?([w]{3}.)([\w]+)(\.)?(\.)?([a-z.]{2,6})?([a-z.]{2,6})(\/)?([\w]+\/?)*$/
+};
+
+var UrlDomainSubDomain = function () {
+  "use strict";
+  this.url;
+  this.domainName;
+  this.subDomainName;
+  this.form;
+  this.submitButton;
+  this.init();
+};
+
+//attached event on submit click
+UrlDomainSubDomain.prototype.init = function (e) {
+  "use strict";
+  this.submitButton = document.getElementById('submitButton');
+  this.form = document.getElementById('urlForm');
+  this.bindEvent();
+};
+
+//method to find domain codomain
+UrlDomainSubDomain.prototype.findDomainSubDomain = function () {
+  "use strict";
+  alert((!RegExp.$6) ? 'Domain: ' + RegExp.$3 + RegExp.$4 + RegExp.$7 : 'Subdomain: ' + RegExp.$3 + ' Domain: ' + RegExp.$6 + RegExp.$7);
+};
+
+//method number whether number matches with the regex or not
+UrlDomainSubDomain.prototype.checkUrlValidity = function (e) {
+  "use strict";
+  this.url = document.getElementById('url');
+  if (!constant.regexUrl.test(this.url.value)) {
+    alert("Wrong Url Entered! Please Enter a new url");
+    e.preventDefault();
+  } else {
+    this.findDomainSubDomain();
+  }
+};
+
+//to bind event on submit
+UrlDomainSubDomain.prototype.bindEvent = function () {
+  "use strict";
+  var that = this;
+  this.submitButton = document.getElementById('submitButton');
+  this.form.addEventListener('submit', function (event) { that.checkUrlValidity(event); }, false);
+};
+
+//method to run javascript after loading whole page (mentioned script in head portion)
+window.onload = function () {
+  "use strict";
+  var urlDomainSubDomain = new UrlDomainSubDomain();
+};
+
+
+
